abbae0e0cf82ce777df72bedb2a1357a362c98a4
YasukoShimada2026 <uni202607_08@telework.to>
2026-09-16 15:41:40 +0900

    更新

---
+++ TeamQuizApp4/AnswerChecker.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamQuizApp3
+{
+    public class AnswerChecker
+    {
+        public bool CheckAnswer(Question q, int selectedIndex)
+        {
+            return q.CorrectIndex == selectedIndex;
+        }
+    }
+}
